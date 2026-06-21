15c95e1b482ac5dbcf7ef28a6285fb787436542a
Berlindi Achica Tama <berlindi24ti@mahasiswa.pcr.ac.id>
2026-06-21 10:01:22 +0700

    mengubah logo dan menghapus tombol tidak aktif

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -77,7 +77,7 @@ export default function Sidebar() {
   return (
     <aside className="flex min-h-screen w-[290px] flex-col border-r bg-[#050505]">
       <div className="px-6 pt-8 text-center">
-        <img src="/img/logo.png" alt="logo" className="mx-auto block w-[220px]" />
+        <img src="/img/logo-bengkel.png" alt="logo" className="mx-auto block w-[220px]" />
       </div>
 
       <div className="mt-7 px-5">
